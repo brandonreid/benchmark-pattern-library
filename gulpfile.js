57b8ccf1ef3cd5bf353c34946863008344069566
Brandon Reid <brandon@usebenchmark.com>
2016-05-24 19:50:36 -0700

    Add gulp dev environment

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,16 @@ var LessAutoprefix = require('less-plugin-autoprefix'),
     autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 var LessPluginCleanCSS = require('less-plugin-clean-css'),
     cleanCSSPlugin = new LessPluginCleanCSS({advanced: true});
+var connect = require('gulp-connect');
 
 
-gulp.task('patterns', function () {
+gulp.task('library', function () {
   gulp.src('less/*.less')
     .pipe(livingcss({
       loadcss: false,
       template: './library-template/template.hbs'
     }))
-    .pipe(gulp.dest('pattern_library'))
+    .pipe(gulp.dest('./pattern_library/'))
 });
 
 gulp.task('less', function () {
@@ -21,5 +22,34 @@ gulp.task('less', function () {
     .pipe(less({
       plugins: [autoprefix, cleanCSSPlugin]
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./pattern_library/css/'));
 });
+
+gulp.task('fonts', function () {
+  gulp.src([
+    './fonts/**/*'
+  ])
+    .pipe(gulp.dest('./pattern_library/fonts/'));
+});
+
+gulp.task('server', function () {
+  connect.server({
+    root: './pattern_library/',
+    port: '9000',
+    livereload: true
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch([
+    'pattern_library/index.html',
+  ], function (event) {
+    return gulp
+      .src(event.path)
+      .pipe(connect.reload());
+  });
+  gulp.watch(['./less/**/*.less'], ['less', 'library']);
+  gulp.watch(['./library-template/**/*.hbs'], ['less', 'library']);
+});
+
+gulp.task('dev', ['less', 'library', 'fonts', 'server', 'watch']);
